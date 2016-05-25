de5a8f88f4a90f2dd17f331054365e8a5ea3c831
oloftus <o@oloft.us>
2016-05-25 14:09:30 +0100

    Updated service registration stuff

--- service-registration/broadcast.sh
+++ service-registration/broadcast.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-mvn exec:java -Dexec.mainClass="co.j6mes.infra.srf.proxy.SRFProxy" -Dexec.args="c2 maps 10080"
+MAPS="maps GPIGGroup2MapsServer/app/ 10080"
+VUI="vui GPIGGroup2ImageVerUI/app/ 10080"
+ALERTS="alerts GPIGGroup2UI/app/ 10080"
+
+mvn exec:java -Dexec.mainClass="co.j6mes.infra.srf.proxy.WebServiceSRFProxy" -Dexec.args="c2 $MAPS $VUI $ALERTS"
 
