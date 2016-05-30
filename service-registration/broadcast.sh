da645e1447ca61ffe5bcebaca6827e80c47d9921
James Thorne <james@jthorne.co.uk>
2016-05-31 00:17:28 +0100

    Updated broadcast script

--- service-registration/broadcast.sh
+++ service-registration/broadcast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 MAPS="maps GPIGGroup2MapsServer/app/ 8080"
-VUI="vui GPIGGroup2ImageVerUI/app/ 8080"
+VUI="vui GPIGGroup2ImageVerificationUI/app/ 8080"
 ALERTS="alerts GPIGGroup2UI/app/ 8080"
 
 mvn exec:java -Dexec.mainClass="co.j6mes.infra.srf.proxy.WebServiceSRFProxy" -Dexec.args="c2 $MAPS $VUI $ALERTS"
