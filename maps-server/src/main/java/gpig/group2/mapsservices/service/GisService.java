8f51e10e2f60d71043e5455ef7accb266d46426c
oloftus <o@oloft.us>
2016-05-30 10:06:44 +0100

    Only update the drone history if the drone position changed

--- maps-server/src/main/java/gpig/group2/mapsservices/service/GisService.java
+++ maps-server/src/main/java/gpig/group2/mapsservices/service/GisService.java
@@ -41,52 +41,49 @@ public class GisService {
 	private Set<OccupiedBuilding> occupiedBuildings = new HashSet<>();
 	private Map<Integer, List<Feature>> floodRiskAreas = new HashMap<>();
 
-
-
 	private Integer lastStrandedPersonId = 0;
-	private Integer lastBuildingOccupancyId = 0;
-
 
 	public synchronized void addOccupiedBuilding(OccupiedBuilding ob) {
+
 		occupiedBuildings.add(ob);
 	}
 
 	public synchronized void addStrandedPerson(StrandedPerson p) {
-		lastStrandedPersonId+=1;
+
+		lastStrandedPersonId += 1;
 		p.setId(lastStrandedPersonId);
 
 		strandedPersons.add(p);
 	}
 
-
 	public synchronized void clearStrandedPerson(Integer id) {
 
 		StrandedPerson found = null;
-		for(StrandedPerson person : strandedPersons) {
-			if(person.getId() == id) {
+		for (StrandedPerson person : strandedPersons) {
+			if (person.getId() == id) {
 				found = person;
 			}
 		}
 
-		if(found != null) {
+		if (found != null) {
 			strandedPersons.remove(found);
 		}
 
 	}
 
 	public void setPersonTask(Integer pid, Integer tid) {
+
 		StrandedPerson found = null;
-		for(StrandedPerson person : strandedPersons) {
-			if(person.getId() == pid) {
+		for (StrandedPerson person : strandedPersons) {
+			if (person.getId() == pid) {
 				found = person;
 			}
 		}
 
-		if(found != null) {
+		if (found != null) {
 			found.setOwningTask(tid);
 		}
 	}
-
 
 	public synchronized void addFloodRiskArea(int riskId, List<Feature> riskMap) {
 
@@ -117,11 +114,10 @@ public class GisService {
 	public synchronized void newDeploymentAreaForPOI(Integer pid, Task deploymentArea) {
 
 		deploymentAreas.put(deploymentAreaCntr, deploymentArea);
-		setPersonTask(pid,deploymentAreaCntr);
+		setPersonTask(pid, deploymentAreaCntr);
 
 		deploymentAreaCntr++;
 	}
-
 
 	public Collection<Layer> getLayers() {
 
@@ -174,8 +170,8 @@ public class GisService {
 
 		for (StrandedPerson strandedPerson : strandedPersons) {
 			Feature spFeature = new Feature();
-			spFeature.setProperty("task",strandedPerson.getOwningTask());
-			spFeature.setProperty("id",strandedPerson.getId());
+			spFeature.setProperty("task", strandedPerson.getOwningTask());
+			spFeature.setProperty("id", strandedPerson.getId());
 
 			Point spPoint = new Point();
 			spPoint.setCoordinates(convertPointToPoint(strandedPerson.getLocation()).getCoordinates());
@@ -280,7 +276,9 @@ public class GisService {
 			drone.setId(droneId);
 		}
 
-		drone.updateLocation(dronePosition);
+		if (!drone.getPosition().equals(dronePosition)) {
+			drone.updateLocation(dronePosition);
+		}
 
 		if (droneWaypoints == null) {
 			drone.setWaypoints(new CoordinateList());
@@ -290,9 +288,8 @@ public class GisService {
 	}
 
 	public void completeTask(int taskId) {
-		
+
 		deploymentAreas.remove(taskId);
 	}
-
 
 }
